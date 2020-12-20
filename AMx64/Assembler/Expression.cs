f46931eba1227f202d91f9b21ac799cb75688bcd
Aleksa Majkic <aleksamcode@gmail.com>
2020-12-20 22:20:31 +0100

    Update.

--- AMx64/Assembler/Expression.cs
+++ AMx64/Assembler/Expression.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using static AMx64.Utility;
 
 namespace AMx64.Assembler
 {
@@ -13,6 +14,38 @@ namespace AMx64.Assembler
             None,
 
             Add, Sub,
+
+            BitAnd, BitOr, 
+            
+            BitNot,
         }
+
+        public Operations Op = Operations.None;
+
+        public Expression Left = null, Right = null;
+
+        private string token = null;
+
+        public string Token
+        {
+            get
+            {
+                return token;
+            }
+
+            set
+            {
+                token = value ?? throw new ArgumentNullException("Token can't be a null value.");
+
+                Op = Operations.None;
+                Left = Right = null;
+            }
+        }
+
+        private UInt64 cachedResult = 0;
+        private bool floatingCR = false;
+
+        public bool IsLeaf => Op == Operations.None;
+        public bool IsEvaluated => Op == Operations.None && Token == null;
     }
 }
--- AMx64/Memory/Registers.cs
+++ AMx64/Memory/Registers.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Runtime.CompilerServices;
 
-
 namespace AMx64
 {
     public partial class SimComputer
