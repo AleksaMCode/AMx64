1f0795dc5fba88093788acebd0b49638d6051170
Aleksa Majkic <aleksamcode@gmail.com>
2021-03-13 14:52:40 +0100

    Bug fixed.

--- AMx64/Simulation.cs
+++ AMx64/Simulation.cs
@@ -42,13 +42,13 @@ namespace AMx64
             // Set asm file full path.
             if (File.Exists(args[0]))
             {
-                if(!args[1].EndsWith(".asm"))
+                if(!args[0].EndsWith(".asm"))
                 {
                     Console.WriteLine("File isn't an .asm file.");
                     return false;
                 }
 
-                if (args[1].Contains('\\'))
+                if (args[0].Contains('\\'))
                 {
                     AsmFilePath = args[0];
                 }
